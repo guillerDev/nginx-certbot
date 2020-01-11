793626a4cf8d190c48c0ff6eae3a488b57129757
guillermo <guilleponfe@gmail.com>
2020-01-11 15:30:44 +0100

    Removed some extra complexity from init-nginxconf.sh.

--- init-nginxconf.sh
+++ init-nginxconf.sh
@@ -5,27 +5,17 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-while getopts d:c:y: option
+while getopts d: option
   do
     case "${option}"
     in
     d) DOMAIN=${OPTARG};;
-    c) CONF_PATH=${OPTARG};;
-    y) YML_PATH=${OPTARG};;
   esac
 done
-
 
 echo "### Stopping services."
 docker-compose stop
 
-echo "### Saving old nginx conf."
-mv -r ./data/nginx ./data/nginx-OLD
-
 echo '### Creating nginx configuration from app.template'
-cp -r CONF_PATH ./data/nginx
 export DOMAIN=$DOMAIN
 envsubst \$DOMAIN < ./data/nginx/app.template > ./data/nginx/app.conf;
-
-echo "### Starting services."
-docker-compose -f docker-compose.override.yml -f $YML_PATH up -d
